62be1ae3d07c52968a70a5121e34759a24bc2668
efecagrisen <efecagrisen@yahoo.com>
2023-07-05 13:02:51 +0300

    commit

---
+++ src/test/java/com/cydeo/firstspringmaven/FirstSpringMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.firstspringmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
